3a64c01d767f53d5edeb2b792a6a2bb7b8bb1358
Ahmed Hazem <ahmed5000hazem@gmail.com>
2023-08-27 23:49:14 +0300

    countries migration and seeder

---
+++ database/seeders/CitiesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CitiesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $cities = [
+            [
+                'name' => 'cairo',
+                'country_id' => 65
+            ]
+        ];
+        
+        DB::insert($cities);
+    }
+}
