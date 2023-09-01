324e5adb706ce18e9b9ed58139ad70a940098f0e
Ahmed Hazem <ahmed5000hazem@gmail.com>
2023-09-01 18:33:40 +0300

    add nullsafety to card component

--- resources/views/components/dashboard/card.blade.php
+++ resources/views/components/dashboard/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => ' bg-white rounded-lg p-6 mt-8 w-'.$width??'1/2']) }} class="w-1/2 bg-white rounded-lg p-6 mt-8">
+<div {{ $attributes->merge(['class' => ' bg-white rounded-lg p-6 mt-8 w-'.$attributes->get('width')??'1/2']) }} class="w-1/2 bg-white rounded-lg p-6 mt-8">
     {{$slot}}
 </div>
