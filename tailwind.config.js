8cabb33aebe793595c580fc276e765482bd4a3bb
Ahmed Hazem <ahmed5000hazem@gmail.com>
2023-09-01 12:04:29 +0300

    fix tailwind and daisyui conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import preset from './vendor/filament/support/tailwind.config.preset'
 
 export default {
+    plugins: [require("@tailwindcss/typography"), require("daisyui")],
     presets: [preset],
     content: [
         './app/Filament/**/*.php',
